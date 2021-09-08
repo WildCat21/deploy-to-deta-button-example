9413ea52a9b0776c4d62b7b0d503e767ddcce878
WildCat21 <85885539+WildCat21@users.noreply.github.com>
2021-09-08 18:23:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 @app.get("/")
 
 def index():
-  deta = Deta("a0nx7pgk_CAsXSD5UjJsWT8xj9nPSAb14xduJ1fUR")
+  deta = Deta("api_key")
   users = deta.Base("tesst")
   users.insert({"key": "test", "bann": "0", })
